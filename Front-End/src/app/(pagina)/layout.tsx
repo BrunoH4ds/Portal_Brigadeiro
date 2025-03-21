698767f1d4843013a0fa64dac08aa0574fb11fe6
Bruno <brunoalburquerque909@gmail.com>
2025-03-21 16:20:39 -0300

    Delete Front-End directory

--- Front-End/src/app/(pagina)/layout.tsx
+++
@@ -1,6 +0,0 @@
-import Main from "@/components/template_home/main";
-
-
-export default function layout({ children }: { children: React.ReactNode }){
-  return <Main>{children}</Main>
-}
--- Front-End/src/components/comp_home/NavBarComp/ButtonsLoginSingUp.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function ButtonsLoginSignUp() {
-  return (
-    <div className="flex gap-3 items-center">
-      <Link href="/login" className="hover:text-blue-900">Entrar</Link>
-    </div>
-  );
-}
